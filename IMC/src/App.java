c4c745b1616ac4efcc90e5c8fb670a4c074cc263
Zet Zed <rodhaste@hotmail.com>
2023-03-06 14:22:53 -0400

    Projeto de IMC

--- IMC/src/App.java
+++ IMC/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe seu peso em kg: ");
+        double peso = sc.nextDouble();
+
+        System.out.println("Informe a sua altura em metros: ");
+        double altura = sc.nextDouble();
+
+        double imc = peso / (altura * altura);
+
+        System.out.printf("O seu IMC é %.2f", imc);
+
+        sc.close();
     }
 }
