b711db44156a63936932d44f0857815d52aa3b0b
Zet Zed <rodhaste@hotmail.com>
2023-03-06 14:46:05 -0400

    Nova atualização.

--- IMC/src/App.java
+++ IMC/src/App.java
@@ -12,7 +12,21 @@ public class App {
 
         double imc = peso / (altura * altura);
 
-        System.out.printf("O seu IMC é %.2f", imc);
+        System.out.printf("O seu IMC é %.2f . ", imc);
+
+        if (imc < 18.5) {
+            System.out.println("Você está abaixo do peso normal.");
+        } else if (imc >= 18.5 && imc < 25) {
+            System.out.println("Seu peso está normal.");
+        } else if (imc >= 25 && imc < 30) {
+            System.out.println("Você está com sobrepeso.");
+        } else if (imc >= 30 && imc < 35) {
+            System.out.println("Você está com obesidade grau 1.");
+        } else if (imc >= 35 && imc < 40) {
+            System.out.println("Você está com obesidade grau 2.");
+        } else {
+            System.out.println("Você está com obesidade grau 3.");
+        }
 
         sc.close();
     }
